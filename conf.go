e3f38716df292473ad8df81b5724da60abcb4bd0
bill mitsakos <bill@rockbot.com>
2026-04-30 09:32:59 -0600

    Update conf.go

--- conf.go
+++ conf.go
@@ -313,7 +313,7 @@ func parse(args []string, namespace string, cfgStruct interface{}, opts *parseOp
 
 		// If the field is marked 'required', check if no value was provided.
 		if field.Options.Required && !foundOverride {
-			return fmt.Errorf("required field %s is missing value", field.Name)
+			return fmt.Errorf("required field %s is missing value", field.Options.EnvName)
 		}
 	}
 
